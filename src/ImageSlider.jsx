0611a7c940774bb78b1deedfa1c10321f6dc5d2c
deliasoare <deliasoare006@yahoo.com>
2023-08-13 20:21:51 +0300

    Add functionality

--- src/ImageSlider.jsx
+++ src/ImageSlider.jsx
@@ -4,13 +4,30 @@ import { HiOutlineArrowRight, HiOutlineArrowLeft } from 'react-icons/hi'
 const ImageSlider = ({images, thumbnails}) => {
     const [currentIndex, setCurrentIndex] = useState(0);
 
+    const onClickLeft = () => {
+        if (currentIndex === 0)
+            setCurrentIndex(images.length - 1);
+        else
+            setCurrentIndex(currentIndex - 1);
+    }
+    const onClickRight = () => {
+        if (currentIndex === images.length - 1)
+            setCurrentIndex(0);
+        else
+            setCurrentIndex(currentIndex + 1);
+    }
+    const onClickPreview = (index) => {
+        return () => {
+            setCurrentIndex(index);
+        }
+    }
     return (
         <div className='relative w-[90%] min-w-[300px] h-[550px] group m-auto my-5 '>
             <div style={{backgroundImage: `url(${images[currentIndex].src})`}} className='bg-cover bg-center rounded-md  duration-500 w-full h-full m-auto' src={images[currentIndex].src}></div>
-            <div className='hidden absolute group-hover:block translate-y-[-50%] top-[50%] left-2 rounded-full bg-black/50 text-white p-3 cursor-pointer'>
+            <div onClick={onClickLeft} className='hidden absolute group-hover:block translate-y-[-50%] top-[50%] left-2 rounded-full bg-black/50 text-white p-3 cursor-pointer'>
                 <HiOutlineArrowLeft  size={25} />
             </div>
-            <div className="hidden absolute group-hover:block translate-y-[-50%] top-[50%] right-2 rounded-full bg-black/50 text-white p-3 cursor-pointer">
+            <div onClick={onClickRight} className="hidden absolute group-hover:block translate-y-[-50%] top-[50%] right-2 rounded-full bg-black/50 text-white p-3 cursor-pointer">
                 <HiOutlineArrowRight size={25}/>
             </div>
             <div className='flex py-4 gap-4 justify-center w-fit m-auto'>
@@ -19,9 +36,9 @@ const ImageSlider = ({images, thumbnails}) => {
                     return (
                         <>
                             {index === currentIndex ?
-                                <div style={{backgroundImage: `url(${images[index].thumbnail})`}} className='bg-cover h-[56px] min-w-[86px] border-[6px] border-black border-solid cursor-pointer'></div>
+                                <div key={index} style={{backgroundImage: `url(${images[index].thumbnail})`}} className='bg-cover h-[56px] min-w-[86px] border-[6px] border-black border-solid cursor-pointer'></div>
                                 : 
-                                <div style={{backgroundImage: `url(${images[index].thumbnail})`}} className='bg-cover h-[50px] min-w-[80px] cursor-pointer'></div>
+                                <div key={index} onClick={onClickPreview(index)} style={{backgroundImage: `url(${images[index].thumbnail})`}} className='bg-cover h-[50px] min-w-[80px] cursor-pointer'></div>
                             }
                         </>
                     );
@@ -31,9 +48,9 @@ const ImageSlider = ({images, thumbnails}) => {
                     return (
                         <>
                             {index === currentIndex ?
-                            <div className='w-[10px] h-[10px] rounded-full bg-black cursor-pointer' > </div>
+                            <div key={index} className='w-[10px] h-[10px] rounded-full bg-black cursor-pointer' > </div>
                             :
-                            <div className='w-[10px] h-[10px] rounded-full border-2 border-solid border-black cursor-pointer' > </div>
+                            <div key={index} onClick={onClickPreview(index)} className='w-[10px] h-[10px] rounded-full border-2 border-solid border-black cursor-pointer' > </div>
                             }
                         </>
                     );
